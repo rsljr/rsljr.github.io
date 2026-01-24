b61b7037f7fd8106e78627fa9732b11789c0e09b
rsljr <r_juniormxpb@hotmail.com>
2026-01-24 20:58:48 +0000

    Deploying to gh-pages from @ rsljr/rsljr.github.io@052053404b6abf084b16a23d9f9455bee3579d81 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -514,6 +514,13 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
+        id: 'social-custom_social',
+        title: 'Custom_social',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://32on64.substack.com/", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
