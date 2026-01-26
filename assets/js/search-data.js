162f2f1f35e6492ca71d5297c6f0f403f00ced17
rsljr <r_juniormxpb@hotmail.com>
2026-01-26 05:29:46 +0000

    Deploying to gh-pages from @ rsljr/rsljr.github.io@efd486be186c870702f24f02f66f4e265d13c261 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -507,13 +507,6 @@ ninja.data = [{
           window.open("https://twitter.com/roneileonel", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
